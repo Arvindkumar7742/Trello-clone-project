fe170ae2120a88bdd127fd702d330ccd383b8f43
Arvindkumar7742 <arvindkumar14032004@gmail.com>
2025-01-21 23:59:58 +0530

    created routing for the boards page and a board page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import { useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+import HomePages from "./pages/HomePages";
+import BoardsPage from "./pages/BoardsPage";
 
 function App() {
-  return <div className="text-5xl">Hello world</div>;
+  return (
+    <>
+      {/* Adding Routes page for the all the pages */}
+      <Routes>
+        <Route path="/" element={<Navigate to="/boards" replace />} />
+        <Route path="/boards" element={<MainLayout />}>
+          <Route index element={<HomePages />} />
+          <Route path="/boards/:id" element={<BoardsPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import HomeIcon from "@mui/icons-material/Home";
+import Box from "@mui/material/Box";
+import { useNavigate } from "react-router-dom";
+
+import logo from "../assets/trello.webp";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleHomeClick = () => {
+    navigate("/boards");
+  };
+
+  return (
+    <AppBar position="static" sx={{ bgcolor: "#026aa7" }}>
+      <Toolbar
+        sx={{
+          maxWidth: "1200px",
+          width: "100%",
+          mx: "auto",
+          position: "relative",
+        }}
+      >
+        {/* Left Section: Home Icon and Boards */}
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="home"
+            onClick={handleHomeClick}
+            sx={{ mr: 1 }}
+          >
+            <HomeIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ cursor: "pointer" }}
+            onClick={handleHomeClick}
+          >
+            Boards
+          </Typography>
+        </Box>
+
+        {/* Center Section: Trello Logo */}
+        <Box
+          sx={{
+            position: "absolute",
+            left: "50%",
+            transform: "translateX(-50%)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={logo}
+            alt="Trello Logo"
+            style={{
+              height: "32px",
+              width: "250px",
+              objectFit: "cover",
+            }}
+          />
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
